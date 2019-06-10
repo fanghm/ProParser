44fd2f777bc70b2d49598aed3d98b50b0ba285fc
fanghm <14533348@qq.com>
2019-06-10 16:31:06 +0800

    minor fix

--- parser.cpp
+++ parser.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <iterator>
+#include "defines.h"
 
 using namespace std;
 
@@ -15,10 +16,11 @@ enum class CSVState {
 
 // globals
 const string HEADER_START_FIELD = "Problem ID";
+const char CSV_DELIMITER = ',';
 CSVState state = CSVState::UnquotedField;
 
 template<typename Out>
-void split(const std::string &s, char delim, Out result) {
+void split(const std::string &s, Out result, char delim = CSV_DELIMITER) {
     std::stringstream ss(s);
     std::string item;
     while (std::getline(ss, item, delim)) {
@@ -28,24 +30,25 @@ void split(const std::string &s, char delim, Out result) {
 
 std::vector<std::string> split(const std::string &s, char delim) {
     std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
+    split(s, std::back_inserter(elems), delim);
     return elems;
 }
 
 void show_fields(std::vector<std::string> &fields) {
-    std::cout << "\t";
+    std::cout << "\t-->[FIELDS]";
     for (std::string val: fields) {
-        std::cout << val.substr(0, 20) << "/";
+        std::cout << val.substr(0, 20) << "||";
     }
-    std::cout << "<<-----\n";
+    std::cout << "<--\n";
 }
 
 void show_table(std::vector<std::vector<std::string>> const &matrix) {
-    size_t n = 0;
+    size_t m = 1, n = 1;
 	for (std::vector<std::string> row: matrix) {
-        std::cout << "\n-->" << ++n << "<--\n";
+        std::cout << "\n-->" << m++ << "<--\n";
 		for (std::string val: row) {
-			std::cout << val.substr(0, 50) << "\n";
+			std::cout << (n > EXPORT_FIELD_NUM ? "__UNDEFINED__" : EXPORT_FIELDS[n++]) << ":\t\t||" << val.substr(0, 50) << "||\n";
+			++n;
 		}
 	}
 }
@@ -54,7 +57,7 @@ void parse_row(const std::string &row, std::vector<std::string> &fields, std::ve
     size_t pos = row.find_first_of('"');
 
     if (state == CSVState::UnquotedField) {
-        split(row, ',', std::back_inserter(fields));
+        split(row, std::back_inserter(fields));
         if ( string::npos != pos) {
             // TODO: remove starting quotation mark of last field
             state = CSVState::QuotedField;
@@ -66,7 +69,7 @@ void parse_row(const std::string &row, std::vector<std::string> &fields, std::ve
             fields.back().append("\n" + row.substr(0, pos)); // remove closing quotation mark
 
             state = CSVState::UnquotedField;
-            split(row.substr(pos+2), ',', std::back_inserter(fields));// skip ",
+            split(row.substr(pos+2), std::back_inserter(fields));// skip ",
         }
     }
 
@@ -76,7 +79,7 @@ void parse_row(const std::string &row, std::vector<std::string> &fields, std::ve
     }
 }
 
-std::vector<std::vector<std::string> > parse_csv(std::string file) {
+std::vector<std::vector<std::string> > parse_csv(std::string& file) {
     std::vector<std::vector<std::string> > table;
     std::vector<std::string> fields;
     std::string row;
@@ -105,11 +108,9 @@ std::vector<std::vector<std::string> > parse_csv(std::string file) {
 }
 
 int main() {
-    string s = "Field, with \"embedded quote\"\n and, new line";
-    std::vector<std::string> fields;
-    split(s, ',', std::back_inserter(fields));
-    for (std::string val: fields) {
-        std::cout << val << "\n";
-    }
+    std::string filename = "./problem-1_no-transfer-no-attach.csv";
+    std::vector<std::vector<std::string>> table = parse_csv(filename);
+    show_table(table);
+
     return 0;
 }
